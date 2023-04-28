22e9b9c8f58680bb0f35a58c22434af0f20e29ba
takumi <takumi_1230@outlook.com>
2023-04-28 11:30:48 +0900

    BingChat対応

--- RequestDest/Bing.py
+++ RequestDest/Bing.py
@@ -1,3 +1,23 @@
 """
 未実装
 """
+import asyncio
+import json
+from EdgeGPT import Chatbot, ConversationStyle
+class bot:
+    def __init__(self) -> None:
+        with open('../cookies.json', 'r') as f:
+            cookies = json.load(f)
+        self.chatbot = Chatbot(cookies=cookies)
+    async def request(self, message):
+        response = await self.chatbot.ask(prompt=message, conversation_style=ConversationStyle.creative, wss_link="wss://sydney.bing.com/sydney/ChatHub")
+        return response['item']['messages'][1]['text']
+
+async def main():
+    chatbot = bot()
+    while True:    
+        print(await chatbot.request(message=input()))
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
